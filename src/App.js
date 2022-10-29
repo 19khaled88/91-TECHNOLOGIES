1b3fede6f2c11bf5e323ca318b2272ae96723973
19khaled88 <kkhaled88hasan@gmail.com>
2022-10-29 23:43:10 +0600

    add new component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
 import './App.css'
+import CompanyList from './components/CompanyList'
 function App() {
   const [companyList, setCompanyList] = useState(null)
   const baseURL = 'http://demo2211087.mockable.io/mock'
@@ -14,6 +15,7 @@ function App() {
   return (
     <div className="container">
       <h3>91 TECHNOLOGIES PRIVATE LIMITED</h3>
+      <CompanyList companyList={companyList} />
     </div>
   )
 }
